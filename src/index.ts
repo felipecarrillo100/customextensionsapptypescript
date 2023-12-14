81c00e4440c466646ebd456fd38f4d6892e050f5
felipecarrillo100 <felipecarrillo100@gmail.com>
2023-12-14 16:17:31 +0100

    Adding tomtom api call

--- src/index.ts
+++ src/index.ts
@@ -117,6 +117,22 @@ window.catex.featureLayer = {
                 }
             } 
         },
+        {
+            label: "Tomtom route",
+            title: "Calculate Route using Tomtom API",
+            action: function(o, callback) {
+                if (typeof callback === "function") {
+                    if (o.features) {
+                        if (o.features.length==2) {
+                            const point1 = o.features[0].shape.focusPoint;
+                            const point2 = o.features[1].shape.focusPoint;
+                            const newCommand = TomTomAPIURL([point1.y, point1.x], [point2.y, point2.x], 'Tomtom route');
+                            window.catex.workspace.emitCommand(newCommand);
+                        }
+                    }                    
+                }
+            } 
+        },
     ],
     onFeatureSelect: [
         {
@@ -267,6 +283,21 @@ window.catex.data = {
                     return {id, type:"Feature", geometry: geom, properties }
                 });
                 return JSON.stringify(geojsonArray);
+            }
+        },
+        {
+            name: "TOMTOM ROUTE",
+            extensions: "json",
+            transform: (dataStr: string) => {
+                const tomtomResponse = JSON.parse(dataStr); 
+                const features = [];                
+                if (tomtomResponse.routes.length > 0 && tomtomResponse.routes[0].legs.length > 0) {                
+                    const coordinates = tomtomResponse.routes[0].legs[0].points.map((point:any)=>[point.longitude, point.latitude]);
+                    const lineString = { type: 'LineString', coordinates};
+                    const feature = {id: 0, type:"Feature", geometry: lineString, properties:tomtomResponse.routes[0].summary};
+                    features.push(feature);
+                }
+                return JSON.stringify(features);
             }
         },
         {
@@ -585,3 +616,220 @@ const SampleCommand = {
       }
     }
   }
+
+  const TomTomAPIURL = (point1: number[], point2: number[], label: string) => ({
+    "action": 10,
+    "parameters": {
+      "action": "MemoryFeatureLayer",
+      "autozoom": true,
+      "layer": {
+        "label": label,
+        "selectable": true,
+        "editable": false,
+        "painterSettings": {
+            "commons": {
+              "balloon": false,
+              "heatmap": {
+                "enabled": false,
+                "maximum": 20,
+                "normalizedGradient": [
+                  {
+                    "level": 0,
+                    "color": "rgba( 0 , 0 , 255, 0.9)"
+                  },
+                  {
+                    "level": 0.25,
+                    "color": "rgba( 0 , 255 , 255, 0.9)"
+                  },
+                  {
+                    "level": 0.5,
+                    "color": "rgba( 0 , 255 , 0, 0.9)"
+                  },
+                  {
+                    "level": 0.75,
+                    "color": "rgba( 255 , 255 , 0, 0.9)"
+                  },
+                  {
+                    "level": 1,
+                    "color": "rgba( 255 , 0 , 0, 0.9)"
+                  }
+                ]
+              }
+            },
+            "default": {
+              "labelStyle": {
+                "background": {
+                  "fill": {
+                    "color": "rgba(138, 200, 255, 0)"
+                  },
+                  "stroke": {
+                    "color": "rgba(138, 200, 255, 0)"
+                  }
+                },
+                "case": "none",
+                "font": {
+                  "bold": true,
+                  "fontFamily": "'Times New Roman', Times, serif",
+                  "fontSize": "14px",
+                  "italic": false,
+                  "underline": false
+                },
+                "labelProperty": "",
+                "stroke": {
+                  "color": "rgba(0,255,255,1)"
+                }
+              },
+              "lineStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "stroke": {
+                  "color": "rgba(255,182,138,1)",
+                  "dashIndex": "solid",
+                  "width": 5
+                }
+              },
+              "pointStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "fill": {
+                  "color": "rgba(138,200,255,0.8)"
+                },
+                "heading": {
+                  "default": "",
+                  "property": "",
+                  "scale": 1
+                },
+                "shape": "gradient-circle",
+                "size": {
+                  "height": 16,
+                  "width": 16
+                },
+                "anchorX": "",
+                "anchorY": "",
+                "stroke": {
+                  "color": "rgb(29,74,118)",
+                  "width": 1
+                },
+                "url": ""
+              },
+              "shapeStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "fill": {
+                  "color": "rgba(255, 0, 0, 0.6)"
+                },
+                "stroke": {
+                  "color": "rgb(255, 255, 0)",
+                  "dashIndex": "solid",
+                  "width": 2
+                },
+                "extruded": {
+                  "type": "Flat",
+                  "height": {
+                    "property": "",
+                    "scale": 1
+                  },
+                  "minHeight": {
+                    "property": "",
+                    "scale": 1
+                  }
+                }
+              }
+            },
+            "painterType": "DefaultPainter",
+            "selected": {
+              "labelStyle": {
+                "background": {
+                  "fill": {
+                    "color": "rgba(138, 200, 255, 0.8)"
+                  },
+                  "stroke": {
+                    "color": "rgba(138, 200, 255, 0.8)"
+                  }
+                },
+                "case": "none",
+                "color": "rgba(0,0,255,1)",
+                "font": {
+                  "bold": true,
+                  "fontFamily": "'Times New Roman', Times, serif",
+                  "fontSize": "14px",
+                  "italic": false,
+                  "underline": false
+                },
+                "labelProperty": "",
+                "stroke": {
+                  "color": "rgba(0,255,255,1)"
+                }
+              },
+              "lineStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "stroke": {
+                  "color": "rgb(0, 128, 256)",
+                  "dashIndex": "solid",
+                  "width": 2
+                }
+              },
+              "pointStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "fill": {
+                  "color": "rgba(255, 200, 255, 0.8)"
+                },
+                "heading": {
+                  "default": "",
+                  "property": "",
+                  "scale": 1
+                },
+                "shape": "gradient-circle",
+                "size": {
+                  "height": 18,
+                  "width": 18
+                },
+                "anchorX": "",
+                "anchorY": "",
+                "stroke": {
+                  "color": "rgba(146,24,48,1)",
+                  "width": 1
+                },
+                "url": ""
+              },
+              "shapeStyle": {
+                "fill": {
+                  "color": "rgba(0, 255, 0, 0.6)"
+                },
+                "stroke": {
+                  "color": "rgb(255, 255, 255)",
+                  "dashIndex": "solid",
+                  "width": 2
+                },
+                "extruded": {
+                  "type": "Flat",
+                  "height": {
+                    "property": "",
+                    "scale": 1
+                  },
+                  "minHeight": {
+                    "property": "",
+                    "scale": 1
+                  }
+                }
+              }
+            }
+          },
+      },
+      "model": {
+        "transformer": "TOMTOM ROUTE",
+        "url": `https://api.tomtom.com/routing/1/calculateRoute/${point1[0]},${point1[1]}:${point2[0]},${point2[1]}/json?&vehicleHeading=90&sectionType=traffic&report=effectiveSettings&routeType=eco&traffic=true&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=120&vehicleCommercial=false&vehicleEngineType=combustion&key=A9R38rS8rPA6NS1ARfwn24mgEMlCCQ9k`,
+        "beforeProxy": `https://api.tomtom.com/routing/1/calculateRoute/${point1[0]},${point1[1]}:${point2[0]},${point2[1]}/json?&vehicleHeading=90&sectionType=traffic&report=effectiveSettings&routeType=eco&traffic=true&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=120&vehicleCommercial=false&vehicleEngineType=combustion&key=A9R38rS8rPA6NS1ARfwn24mgEMlCCQ9k`,
+        method: "GET",
+        "format": "GeoJSON",
+        "crs": "CRS:84",
+        "swapAxes": false,
+        "credentials": false,
+        "requestHeaders": {
+            "Content-Type": "application/json"
+        }
+      }
+    }
+  })
